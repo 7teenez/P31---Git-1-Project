591349563299f3c69c5bbb977ff4aff6031f5bb9
Radch_wi54@student.itstep.org <Radch_wi54@student.itstep.org>
2024-01-20 13:33:13 +0200

    Добавил вычесление площади окружности

--- P31 - Git 1 Project/main.cpp
+++ P31 - Git 1 Project/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+	const double pi = 3.14159265;
+	double r;
+	cout << "Enter circle radius (in cm)";
+	cin >> r;
 
-	cout << "Hello world!" << endl;
+	cout << "S = " << pi * pow(r, 2) << " cm^2" << endl;
+
 	return 0;
 }
